0280716cbcfa6bd44de058945e120088f1e23521
Dan Arrington <dan.arrington@gmail.com>
2016-02-23 07:25:34 -0800

    Added picks table to player page

--- app/facades/player_team_facade.rb
+++ app/facades/player_team_facade.rb
@@ -1,18 +1,20 @@
 class PlayerTeamFacade
 
-  attr_accessor :current_pick, :current_picker, :any_picks_left, 
-    :your_next_pick
+  attr_accessor :current_pick, :current_picker, :any_picks_left,
+    :your_next_pick, :picks
 
   
   def initialize(draft_id, player)
     draft = Draft.find(draft_id)
     @current_pick = draft.current_pick.number.ordinalize
     @current_picker = draft.current_pick.player.name
-    next_pick = draft.next_pick_for(player)
-    @any_picks_left = !next_pick.nil? 
+    @picks = player.picks_for(draft)
+    @any_picks_left = draft.next_pick_for(player)
+
     if @any_picks_left
       @your_next_pick = draft.next_pick_for(player).number.ordinalize
     end
   end
+
 
 end
