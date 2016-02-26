c4af539975506a96d2c1a4d5bcef41ab40ed8fdd
Dan Arrington <dan.arrington@gmail.com>
2016-02-26 06:49:06 -0800

    Started end of draft work

---
+++ spec/features/finish_draft_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'Making the last pick of a draft' do
+
+  let!(:draft) {create(:last_pick_draft)}
+  before(:each) {ActionMailer::Base.deliveries.clear}
+
+  it 'Displays the draft recap and sends the end of draft email' do
+    player = draft.current_pick.player 
+    visit pick_path(draft, player, player.token)
+    click_button "Pick"
+
+    #TODO expect to see draft recap
+    
+    expect(ActionMailer::Base.deliveries.count).to eq draft.players.count
+
+  end
+
+
+
+
+end
